84b69980ee055346bcf2d4cc94146197e01f7089
효정이 <w2029@e-mirim.hs.kr>
2021-09-14 12:01:34 +0900

    흔한 예외

---
+++ except/except1.py
@@ -0,0 +1,11 @@
+'''
+java
+try {
+    // 예외 처리가 의심되는 코드
+}catch(FileNotFoundException e){
+    // FileNotFoundException 처리하는 코드
+}catch{
+    // 예외가 났을 때 처리하는 코드
+} finally {
+}   // 무조건 실행헤야하는 코드
+'''
