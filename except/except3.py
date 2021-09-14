cdcd1c1e2da2bb08bc9784f47a027eaddcc53d92
효정이 <w2029@e-mirim.hs.kr>
2021-09-14 12:16:20 +0900

    파이썬 예외처리

---
+++ except/except3.py
@@ -0,0 +1,14 @@
+list1 = [1, 5, 7]
+try:
+    print(list1[0])
+    print(list1[1])
+    print(list1[2])
+    # print(list1[3])     # IndexError
+except IndexError as e:
+    print(e)
+except:
+    pass
+else:
+    print('성공!!!🎆🎆')
+finally:
+    print('꼭 실행해야하는 코드')
