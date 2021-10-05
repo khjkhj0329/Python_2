b42d5a38d9350212396e43287f25efb7fe7c055e
효정이 <w2029@e-mirim.hs.kr>
2021-10-05 12:25:41 +0900

    load binary, filesize

--- file handler/sava_binary.py
+++ file handler/sava_binary.py
@@ -1,4 +1,4 @@
-with open('brnary.bin', 'wb') as f:
+with open('binary.bin', 'wb') as f:
     # data = b'\xea\xb0\x80'  #1110_1011_0000_1000_0000
     data = bytes([255, 0, 127]) #11111111_00000000_01111111
     f.write(data)
