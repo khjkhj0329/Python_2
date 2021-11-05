13c6de27c9e6d19e5ae0b36884c7cc50c97cebfe
효정이 <w2029@e-mirim.hs.kr>
2021-11-05 10:19:10 +0900

    tictactoe GUI init GUI

---
+++ tictactoe/tictactoe_GUI.py
@@ -0,0 +1,24 @@
+import tkinter
+
+from tictactoe_game_engine import TictactoeGameEngine
+
+class TictactoeGUI:
+    def __init__(self):
+        self.game_engine = TictactoeGameEngine()
+        self.init_GUI()
+
+    def init_GUI(self):
+        self.CANVAS_SIZE = 300  # 창 크기
+        self.root = tkinter.Tk()
+        self.root.title('틱택토')
+        self.root.geometry(f'{self.CANVAS_SIZE}x{self.CANVAS_SIZE}')    #300*300
+        self.root.resizable(width=False, height=False)  # 크기가 조절이 안된다.
+
+    def click_handler(self, event):
+        pass
+
+    def draw_board(self):
+        pass
+
+if __name__ == '__main__':
+    ttt_GUI = TictactoeGUI()
